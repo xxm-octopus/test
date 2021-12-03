3fcf130c45ac7c60ef578f2ba717cd0bc5324803
24494560@qq.com <24494560@qq.com>
2021-12-03 10:47:54 +0800

    V1

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"go/scanner"
+	"go/token"
 	"test/myFunc"
 )
 
@@ -14,5 +17,21 @@ func main() {
 	// reptile.ReptilePic(imgUrls)
 
 	_ = myFunc.PrintSyntaxTrees("D:/GoProjects/test.go")
+
+	var src = []byte(`println("你好，世界")`)
+
+	var fset = token.NewFileSet()
+	var file = fset.AddFile("D:/GoProjects/test.go", fset.Base(), len(src))
+
+	var s scanner.Scanner
+	s.Init(file, src, nil, scanner.ScanComments)
+
+	for {
+		pos, tok, lit := s.Scan()
+		if tok == token.EOF {
+			break
+		}
+		fmt.Printf("%s\t%s\t%q\n", fset.Position(pos), tok, lit)
+	}
 
 }
