d4830a9f367b04916e48f39a4b8b6ea18c52c012
24494560@qq.com <24494560@qq.com>
2021-12-03 10:47:39 +0800

    xxm

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"strconv"
 	"test/myFunc"
-	"test/reptile"
 )
 
 func main() {
-	var imgUrls []string
-	var url string
-	for i := 1; i < 27; i++ {
-		url = `https://www.bizhizu.cn/shouji/tag-%E5%8F%AF%E7%88%B1/` + strconv.Itoa(i) + ".html"
-		imgUrls = append(imgUrls, url)
-	}
-	reptile.ReptilePic(imgUrls)
+	// var imgUrls []string
+	// var url string
+	// for i := 1; i < 27; i++ {
+	// 	url = `https://www.bizhizu.cn/shouji/tag-%E5%8F%AF%E7%88%B1/` + strconv.Itoa(i) + ".html"
+	// 	imgUrls = append(imgUrls, url)
+	// }
+	// reptile.ReptilePic(imgUrls)
 
 	_ = myFunc.PrintSyntaxTrees("D:/GoProjects/test.go")
 
